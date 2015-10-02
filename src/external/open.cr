914ea8b2e044c987dc6d84519a324721a22acda9
rhysd <lin90162@yahoo.co.jp>
2015-10-03 01:22:25 +0900

    add document

--- src/external/open.cr
+++ src/external/open.cr
@@ -1,4 +1,7 @@
+# Module to deal with external stuffs in Crystal
 module External
+
+  # :nodoc:
   private def self.opener_darwin(app)
     if app
       "open -a '#{app}'"
@@ -7,6 +10,7 @@ module External
     end
   end
 
+  # :nodoc:
   private def self.opener_windows(app)
     if app
       "start '' '#{app}'"
@@ -15,6 +19,7 @@ module External
     end
   end
 
+  # :nodoc:
   private def self.opener_others(app)
     if app
       app
@@ -23,6 +28,15 @@ module External
     end
   end
 
+  # Returns the command string to open something externally.
+  #
+  # ```
+  #   External::opener
+  #   External::opener("Google Chrome")
+  #   External::opener("Google Chrome", :darwin)
+  #   External::opener("Google Chrome", :windows)
+  #   External::opener("Google Chrome", :linux)
+  # ```
   def self.opener(app = nil, platform = nil)
     if platform
       return case platform
@@ -44,7 +58,18 @@ module External
     end
   end
 
-  def self.open(uri: String, app = nil, platform = nil)
-    system("#{opener(app, platform)} '#{uri}'")
+  # Opens something externally in manner of your OS.
+  # In OS X, it uses 'open' command.
+  # In Windows, it uses 'start' command.
+  # In Linux or other *nix OS, it uses 'xdg-open' command included in this repository.
+  #
+  # ```
+  #   External::open "http://crystal-lang.org/"
+  #   External::open "file:///Users/foo/Documents/somedoc.pdf"
+  #   External::open "../../README.md"
+  #   External::open "musics/bar.mp3"
+  # ```
+  def self.open(something: String, app = nil, platform = nil)
+    system("#{opener(app, platform)} '#{something}'")
   end
 end
